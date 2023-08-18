82c0b40fedfcb048f6a37457b5f1e495289b22b9
Matt Weber <matt@badecho.com>
2023-08-18 18:48:27 -0500

    Updating msdfgen cmake options

--- ports/msdf-atlas-gen/portfile.cmake
+++ ports/msdf-atlas-gen/portfile.cmake
@@ -10,10 +10,26 @@ if(NOT EXISTS "${SOURCE_PATH}/.git")
 	)
 endif()
 
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        openmp MSDFGEN_USE_OPENMP
+        geometry-preprocessing MSDFGEN_USE_SKIA
+        tools MSDFGEN_BUILD_STANDALONE
+    INVERTED_FEATURES
+        extensions MSDFGEN_CORE_ONLY
+)
+
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"  
   PREFER_NINJA
-  OPTIONS -DMSDF_ATLAS_INSTALL=ON -DMSDF_ATLAS_BUILD_STANDALONE=OFF
+  OPTIONS -DMSDF_ATLAS_INSTALL=ON 
+          -DMSDF_ATLAS_BUILD_STANDALONE=OFF
+          -DMSDFGEN_USE_VCPKG=ON
+          -DMSDFGEN_VCPKG_FEATURES_SET=ON
+                  ${FEATURE_OPTIONS}
+  MAYBE_UNUSED_VARIABLES
+      MSDFGEN_VCPKG_FEATURES_SET
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
